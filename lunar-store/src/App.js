75c68900e92d0b5b22f203d314fd64b70d9d34f6
Laurensius Joshua Anrico Agustinus <joshua.anrico30@gmail.com>
2020-05-15 00:59:52 +0700

    Navbar slesai

--- lunar-store/src/App.js
+++ lunar-store/src/App.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
+import {Switch,Route} from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from './components/Navbar';
-import ProducList from './components/ProducList';
+import ProductList from './components/ProductList';
 import Details from './components/Details';
 import Cart from './components/Cart';
 import Default from './components/Default';
@@ -13,10 +14,14 @@ class App extends Component {
     return (
       <React.Fragment>
         <Navbar></Navbar>
-        <ProducList></ProducList>
-        <Details></Details>
-        <Cart></Cart>
-        <Default></Default>
+
+        <Switch>
+          <Route exact path='/' component={ProductList} ></Route>
+          <Route path='/details' component={Details} ></Route>
+          <Route path='/cart' component={Cart} ></Route>
+          <Route component={Default} ></Route>
+        </Switch>
+
       </React.Fragment>
     );
   }
--- lunar-store/src/components/Navbar.js
+++ lunar-store/src/components/Navbar.js
@@ -1,11 +1,54 @@
 import React, { Component } from 'react';
+import {Link} from 'react-router-dom';
+import logo from '../logo.svg'
+import 'font-awesome/css/font-awesome.min.css';
 
 export default class Navbar extends Component {
 	render() {
 		return (
-			<div>
-				<h3>test nvabar</h3>
-			</div>
+
+			<nav class="navbar navbar-expand-lg navbar-light bg-light">
+				{/* Logo dan nama Store */}
+				<a class="navbar-brand" href="#">
+					<img src={logo} width="50" height="30" class="d-inline-block align-top" alt="" />
+				    Lunar Store
+				</a>
+			  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+			    <span class="navbar-toggler-icon"></span>
+			  </button>
+				
+				{/* Navbar yg kiri */}
+			  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+			    <ul class="navbar-nav mr-auto">
+			      <li class="nav-item active">
+			        <a class="nav-link" href="/">Home <span class="sr-only">(current)</span></a>
+			      </li>
+			      <li class="nav-item">
+			        <a class="nav-link" href="#">Link</a>
+			      </li>
+			      <li class="nav-item dropdown">
+			        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+			          Dropdown
+			        </a>
+			        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+			          <a class="dropdown-item" href="#">Action</a>
+			          <a class="dropdown-item" href="#">Another action</a>
+			          <div class="dropdown-divider"></div>
+			          <a class="dropdown-item" href="#">Something else here</a>
+			        </div>
+			      </li>
+			      <li class="nav-item">
+			        <a class="nav-link disabled" href="#">Disabled</a>
+			      </li>
+			    </ul>
+			    
+			    {/* Navbar yg kanan */}
+				<a href="/Cart" class="btn btn-outline-success my-2 my-sm-0">
+					<i className="fas fa-cart-plus" /> My Cart
+				</a>
+
+			  </div>
+			</nav>
 		);
 	}
 }
