c4fe1426daa9e4afc2867b7e19e02671d8ec4fbe
febiolagatha <febiolagatha@gmail.com>
2020-05-16 15:22:45 +0700

    Update CartItem.js
    Update CartItem.js

--- lunar-store/src/components/Cart/CartItem.js
+++ lunar-store/src/components/Cart/CartItem.js
@@ -1,7 +1,22 @@
 import React from 'react';
 
-export default function CartItem() {
+export default function CartItem(item,value) {
+  const{id,title,img,price,total,count} = item;
+  const{inCrement,dercement,removeItem} = value;
   return (
-    <div>this is a cart item</div>;
-  )
+    <div class="row my-1 text-capitalize text-center">
+      <div class="col-10 mx-auto col-lg-2">
+      <img
+      src={img}
+      style={{width: "5rem", height:"5rem"}}
+      class="img-fluid"
+      alt="product"
+      />
+    </div>
+    <div class="col-10 mx-auto col-lg-2">
+    </div>
+    <div class="col-10 mx-auto col-lg-2">
+    </div>
+    <div>
+  );
 }
