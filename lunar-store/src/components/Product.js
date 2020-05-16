1ba3c2165c2dc8119d63825bbf5b630af57463fe
febiolagatha <febiolagatha@gmail.com>
2020-05-16 13:31:10 +0700

    Cart & get item product
    Cart & getItem Product

--- lunar-store/src/components/Product.js
+++ lunar-store/src/components/Product.js
@@ -10,17 +10,24 @@ export default class Product extends Component {
 		const {id, title, img, price, inCart} = this.props.product;
 		return (
 			<div class="card bg-light mb-3" style={{width: "18rem"}}>
+				<ProductConsumer>
+					{(value) => (
+						class="img-container p-5"
+							onClick={() =>
+								value.handleDetail(id)}
+					>
 			  <img class="card-img-top" src={img} alt="Card image cap" />
 			  <div class="card-body">
 			    <h5 class="card-title">{title}</h5>
 			    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
 			    <a href="/details" class="btn btn-primary">Go somewhere</a>
-			    <button class="btn btn-outline-warning" disabled={inCart ? true : false} onClick={() => {console.log('rere');}} >
+			    <button class="btn btn-outline-warning" disabled={inCart ? true : false} onClick={() => {value.addToCart(id);}} >
 					{inCart?(<p class="text-capitalize mb-0" disabled>In Cart</p>):(<FaShoppingCart size={25}></FaShoppingCart>)}
 			    </button>
 			    <p>$ {price}</p>
 			  </div>
-			</div>
+			</div> )}
+		</ProductConsumer>
 		);
 	}
 }
