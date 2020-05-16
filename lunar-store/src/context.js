123eb9932401c988f676ccb02759ace3bd2b9e72
febiolagatha <febiolagatha@gmail.com>
2020-05-16 14:20:31 +0700

    cartSubTotal, cartTax dan cartTotal
    Menambahkan cartSubTotal, cartTax dan cartTotal pada Cart.

--- lunar-store/src/context.js
+++ lunar-store/src/context.js
@@ -9,6 +9,9 @@ class ProductProvider extends Component {
     cart:[],
     modalOpen:false,
     modalProduct:detailProduct,
+    cartSubTotal: 0,
+    cartTax: 0,
+    cartTotal: 0,
   };
   componentDidMount(){
     this.setProducts();
@@ -62,7 +65,9 @@ closeModal = () =>{
     return {modalOpen:false};
   });
 };
-closeModal = id =>
+inCrement = (id) =>{
+  console.log('this is increment method');
+}
   render() {
     return (
       <ProductContext.Provider value={{
