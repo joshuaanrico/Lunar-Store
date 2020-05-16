e4bcdfb5d20cad8dc0e14cbbb0805fd127db176d
febiolagatha <febiolagatha@gmail.com>
2020-05-16 15:16:27 +0700

    CartList fix
    CartList fix

--- lunar-store/src/components/Cart/CartList.js
+++ lunar-store/src/components/Cart/CartList.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import CartItem from './CartItem';
-export default function CartList() {
+export default function CartList({value}) {
+  const { cart } = value;
+  console.log(value,cart);
   return (
-    <div>hello from cart CartList
-    <CartItem></CartItem>
-    </div>;
-  )
+    <div class="container-fluid">
+      {cart.map(item=>{
+        return <CartItem key-{item.id} item={item}
+        value={value}/>;
+      })}
+    </div>
+  );
 }
