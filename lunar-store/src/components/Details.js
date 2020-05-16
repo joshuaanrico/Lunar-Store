256693eba88497dc638d16611b3e32e98a59d064
Laurensius Joshua Anrico Agustinus <joshua.anrico30@gmail.com>
2020-05-17 03:18:43 +0700

    backend mongodb, profile, login, register

--- lunar-store/src/components/Details.js
+++ lunar-store/src/components/Details.js
@@ -31,7 +31,7 @@ export default class Details extends Component {
 									</h4>
 									<h4 class="text-blue">
 										<strong>
-											Price: <span>Rp. </span> {priceshow}
+											Price: <span>Rp. </span> {price}
 										</strong>
 									</h4>
 									<p class="text-capitalize font-weight-bold mt-3 mb-0">
